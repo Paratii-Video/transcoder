40d9531a8702e7cea230679766b71b2a9169fae9
Yahya <ya7yaz@gmail.com>
2018-02-07 19:17:11 +0200

    debugging

--- src/node.js
+++ src/node.js
@@ -129,12 +129,14 @@ class PublisherNode extends EventEmitter {
             { hash: args.hash,
               err: JSON.stringify(err)
             })
+          console.log('sending pin:error ', msg)
           this.ipfs.protocol.network.sendMessage(peerId, msg, (err) => {
             if (err) return console.log('err: ', err)
             console.log('paratii protocol msg sent: ', args.hash)
           })
         } else {
           let msg = this.ipfs.protocol.createCommand('pin:done', { hash: args.hash })
+          console.log('sending pin:done ', msg)
           this.ipfs.protocol.network.sendMessage(peerId, msg, (err) => {
             if (err) return console.log('err: ', err)
             console.log('paratii protocol msg sent: ', args.hash)
