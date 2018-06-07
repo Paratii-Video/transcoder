c88daced5cf76b70ff1a6a3292df5bb08f88f533
Yahya <ya7yaz@gmail.com>
2018-06-07 21:11:58 +0200

    initial resize api

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -2,11 +2,15 @@
 const os = require('os')
 const fs = require('fs')
 const path = require('path')
+const crypto = require('crypto');
 const express = require('express')
 const fileUpload = require('express-fileupload')
 const glob = require('glob')
 const eachLimit = require('async/eachLimit')
 const once = require('once')
+const request = require('request')
+const ffmpeg = require('fluent-ffmpeg')
+const pump = require('pump')
 
 const router = express.Router()
 // const resumable = require('./resumable-node.js')('/tmp/resumable.js/')
@@ -56,45 +60,6 @@ module.exports = (node) => {
     res.status(200).send('check logs')
   })
 
-  // router.post('/transcode/:hash', (req, res, next) => {
-  //   // res.json({Message: 'Not implemented yet', code: 0})
-  //   let hash = req.params.hash
-  //   req.pipe(fs.createWriteStream(path.join(os.tmpdir(), 'paratii-ipfs-' + hash)))
-  //   req.on('end', () => {
-  //     node.ipfs.upload([path.join(os.tmpdir(), 'paratii-ipfs-' + hash)], (err, hashes) => {
-  //       if (err) {
-  //         return res.status(500).send(err)
-  //       }
-  //       console.log('hashes: ', hashes)
-  //       if (hashes && hashes[0]) {
-  //         if (hashes[0].hash === req.params.hash) {
-  //           return res.status(200).json({hash: hash, status: 'done'})
-  //         } else {
-  //           res.status(406).json({
-  //             err: 'hash mismatch',
-  //             expected: req.params.hash,
-  //             actual: hashes[0].hash
-  //           })
-  //         }
-  //       } else {
-  //         return res.status(400).send('couldn\'t produce ipfs hash')
-  //       }
-  //     })
-  //   })
-  //
-  //   // console.log('files: ', req.files)
-  //   // if (!req.files) {
-  //   //   return res.status(400).send('No files were uploaded.')
-  //   // }
-  //   //
-  //   // let file = req.files.originVideo
-  //   // file.mv(path.join(os.tmpdir(), 'paratii-ipfs-' + hash), (err) => {
-  //   //   if (err) {
-  //   //     return res.status(500).send(err)
-  //   //   }
-  //   // })
-  // })
-
   router.post('/transcode/:hash', (req, res, next) => {
     let hash = req.params.hash
     console.log('req.body: ', req.body)
@@ -119,116 +84,11 @@ module.exports = (node) => {
             console.log('got hashes: ', hashes)
             res.send('ok')
           })
-          // if (!ipfsStream) {
-          //   ipfsStream = node.ipfs.ipfs.files.addReadableStream()
-          // }
-          // let writeIt = fs.createWriteStream(path.join(os.tmpdir(), 'paratii-ipfs' + hash))
-          //
-          // eachLimit(files, 1, (file, callback) => {
-          //   callback = once(callback)
-          //   let s = fs.createReadStream(file).pipe(writeIt)
-          //   s.once('error', (err) => {
-          //     console.log('readStream Err: ', file, err)
-          //     return callback(err)
-          //   })
-          //
-          //   s.once('close', () => {
-          //     return callback()
-          //   })
-          // }, (err) => {
-          //   if (err) throw err
-          //   res.status(200).send('ok')
-          // })
         })
       } else {
         return res.status(200).send('ok')
       }
     })
-    // if (req.body && req.body.resumableFilename) {
-    //   if (!ipfsStream) {
-    //     ipfsStream = node.ipfs.ipfs.files.addReadableStream()
-    //   }
-    //
-    //   if (!lastChunk[req.body.resumableFilename]) {
-    //     lastChunk[req.body.resumableFilename] = 0
-    //   }
-    //
-    //   if (lastChunk[req.body.resumableFilename] === parseInt(req.body.resumableTotalChunks) &&
-    //       req.body.resumableChunkNumber !== req.body.resumableTotalSize) {
-    //     lastChunk[req.body.resumableFilename] = 0
-    //   }
-    //
-    //   if (lastChunk[req.body.resumableFilename] + 1 === parseInt(req.body.resumableChunkNumber)) {
-    //     ipfsStream.once('data', (file) => {
-    //       console.log('ipfsStream: ', file)
-    //       try {
-    //         res.status(200).send('ok')
-    //       } catch (e) {
-    //         console.log('express err: ', e.msg)
-    //       }
-    //     })
-    //     console.log('writing chunk ', req.body.resumableChunkNumber)
-    //     ipfsStream.write({
-    //       path: req.body.resumableFilename,
-    //       content: Buffer.from(req.files.file.data)
-    //     })
-    //
-    //     lastChunk[req.body.resumableFilename]++
-    //   } else {
-    //     try {
-    //       res.status(405).send('nonce mismatch')
-    //     } catch (e) {
-    //       console.log('express err: ', e.msg)
-    //     }
-    //   }
-    //   // -----------------------------------------------------------------------
-    //   // if (!streams[req.body.resumableFilename]) {
-    //   //   streams[req.body.resumableFilename] = fs.createWriteStream(path.join(os.tmpdir(), 'paratii-ipfs-' + hash))
-    //   //
-    //   //   streams[req.body.resumableFilename].on('error', (err) => {
-    //   //     if (err) {
-    //   //       console.error('stream error : ', err)
-    //   //       return res.status(500).send(err)
-    //   //     }
-    //   //   })
-    //   //
-    //   //   streams[req.body.resumableFilename].once('close', () => {
-    //   //     delete streams[req.body.resumableFilename]
-    //   //   })
-    //   // }
-    //   //
-    //   // if (req.body.resumableChunkNumber === req.body.resumableTotalChunks) {
-    //   //   streams[req.body.resumableFilename].end(req.files.file.data, () => {
-    //   //     console.log('ending stream')
-    //   //     console.log(path.join(os.tmpdir(), 'paratii-ipfs-' + hash))
-    //   //     node.ipfs.upload([path.join(os.tmpdir(), 'paratii-ipfs-' + hash)], (err, hashes) => {
-    //   //       if (err) {
-    //   //         return res.status(500).send(err)
-    //   //       }
-    //   //       console.log('hashes: ', hashes)
-    //   //       if (hashes && hashes[0]) {
-    //   //         if (hashes[0].hash === hash) {
-    //   //           res.status(200).send('done')
-    //   //         } else {
-    //   //           console.log('hash mismatch', hash, hashes[0].hash)
-    //   //           res.status(406).send('hash mismatch ' + hash + ' ' + hashes[0].hash)
-    //   //         }
-    //   //       } else {
-    //   //         res.status(400).send('couldn\'t produce ipfs hash')
-    //   //       }
-    //   //     })
-    //   //   })
-    //   // } else {
-    //   //   streams[req.body.resumableFilename].write(req.files.file.data, () => {
-    //   //     // if (req.body.resumableChunkNumber === req.body.resumableTotalChunks) {
-    //   //     // } else {
-    //   //     // }
-    //   //
-    //   //     return res.status(200).send('ok')
-    //   //   })
-    //   // }
-    //   // -----------------------------------------------------------------------
-    // }
     // req.body:  { resumableChunkNumber: '29',
     //   resumableChunkSize: '1048576',
     //   resumableCurrentChunkSize: '1757886',
@@ -252,6 +112,39 @@ module.exports = (node) => {
     //   res.send(status)
     // })
     // res.status(200).send('ok')
+  })
+
+  router.get('/thumbnail/resize', (req, res, next) => {
+    let imgUrl = req.query.url
+    let size = req.query.size
+    let hash = crypto.createHash('sha256').digest(imgUrl)
+    console.log(imgUrl, size, hash.toString('hex'))
+
+    let stream = fs.createWriteStream(path.join(os.tmpdir(), 'img-js-' + hash.toString('hex') + path.extname(imgUrl)))
+    let outputFilename = path.join(os.tmpdir(), 'resized-img-js-' + hash.toString('hex') + path.extname(imgUrl))
+
+    request.get(imgUrl)
+      .on('error', (err) => {
+        res.sendStatus(500).send(err)
+      })
+      .on('end', () => {
+        console.log('stream ended!', stream.path)
+
+        ffmpeg(stream.path)
+          .addOption(`-vf scale=${size}`)
+          .on('end', () => {
+            console.log(stream.path, ':', size, '\t DONE')
+            // res.send(200)
+            // res.send()
+            let f = fs.createReadStream(outputFilename)
+            pump(f, res, (err) => {
+              if (err) console.log('err: ', err)
+            })
+          })
+          .save(path.join(os.tmpdir(), 'resized-img-js-' + hash.toString('hex') + path.extname(imgUrl)))
+          .run()
+      })
+      .pipe(stream)
   })
 
   node.api.use((req, res, next) => {
